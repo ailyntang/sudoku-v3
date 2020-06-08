1ae43c42e0f38269b9dab2be02f281a19ea92a8c
Ai-Lyn Tang <ailyntang@gmail.com>
2020-06-08 21:46:52 +1000

    Add base vision kit scanning capability using the camera

--- sudoku-v3/ViewController.swift
+++ sudoku-v3/ViewController.swift
@@ -14,52 +14,120 @@ final class ViewController: UIViewController {
 
     // MARK: Properties
     
-    private lazy var titleLabel = UILabel()
     private lazy var button: UIButton = {
         let button = UIButton()
-        button.setTitle("Upload screenshot", for: .normal)
-//        button.setTitleColor(.black, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Scan", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18.0)
+        button.titleLabel?.textColor = .white
+        button.layer.cornerRadius = 7.0
+        button.backgroundColor = UIColor.systemIndigo
         button.addTarget(self, action: #selector(ViewController.buttonAction(_:)), for: .touchUpInside)
         return button
     }()
+    
+    private lazy var ocrTextView: UITextView = {
+        let ocrTextView = UITextView()
+        ocrTextView.translatesAutoresizingMaskIntoConstraints = false
+        ocrTextView.layer.cornerRadius = 7.0
+        ocrTextView.layer.borderWidth = 1.0
+        ocrTextView.layer.borderColor = UIColor.systemTeal.cgColor
+        ocrTextView.font = .systemFont(ofSize: 16.0)
+        return ocrTextView
+    }()
+    
+    private lazy var scanImageView: UIImageView = {
+        let view = UIImageView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.layer.cornerRadius = 7.0
+        view.layer.borderWidth = 1.0
+        view.layer.borderColor = UIColor.systemIndigo.cgColor
+        view.backgroundColor = UIColor.init(white: 1.0, alpha: 0.1)
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    private lazy var ocrRequest = VNRecognizeTextRequest(completionHandler: nil)
     
     // MARK: Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        titleLabel.text = "HEEELLLOO"
-        
         setupConstraints()
+        configureOCR()
     }
     
     // MARK: Private
     
     private func setupConstraints() {
-        view.addSubview(titleLabel)
-        
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: Layout.padding).isActive = true
-        titleLabel.topAnchor.constraint(equalTo: view.topAnchor, constant: Layout.padding).isActive = true
         
         view.addSubview(button)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -Layout.padding).isActive = true
-        button.topAnchor.constraint(equalTo: view.topAnchor, constant: Layout.padding).isActive = true
+        view.addSubview(ocrTextView)
+        view.addSubview(scanImageView)
+        
+        let padding: CGFloat = 16.0
+        NSLayoutConstraint.activate([
+        button.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+        button.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+        button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -padding),
+        button.heightAnchor.constraint(equalToConstant: 50),
+            
+        ocrTextView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+        ocrTextView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+        ocrTextView.bottomAnchor.constraint(equalTo: button.topAnchor, constant: -padding),
+        ocrTextView.heightAnchor.constraint(equalToConstant: 200),
+            
+        scanImageView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: padding),
+        scanImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: padding),
+        scanImageView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -padding),
+        scanImageView.bottomAnchor.constraint(equalTo: ocrTextView.topAnchor, constant: -padding)
+        ])
     }
     
     @objc private func buttonAction(_ sender: UIButton) {
-        print("button tapped")
-        guard VNDocumentCameraViewController.isSupported else {
-            print("vkit not supported")
-            return
-        }
+        guard VNDocumentCameraViewController.isSupported else { return }
 
-        print("vision kit is supported")
         let controller = VNDocumentCameraViewController()
         controller.delegate = self
 
         present(controller, animated: true)
+    }
+    
+    private func processImage(_ image: UIImage) {
+        guard let cgImage = image.cgImage else { return }
+
+        ocrTextView.text = ""
+        button.isEnabled = false
+        
+        let requestHandler = VNImageRequestHandler(cgImage: cgImage, options: [:])
+        do {
+            try requestHandler.perform([self.ocrRequest])
+        } catch {
+            print(error)
+        }
+    }
+    
+    private func configureOCR() {
+        ocrRequest = VNRecognizeTextRequest { (request, error) in
+            guard let observations = request.results as? [VNRecognizedTextObservation] else { return }
+            
+            var ocrText = ""
+            for observation in observations {
+                guard let topCandidate = observation.topCandidates(1).first else { return }
+                
+                ocrText += topCandidate.string + "\n"
+            }
+            
+            DispatchQueue.main.async {
+                self.ocrTextView.text = ocrText
+                self.button.isEnabled = true
+            }
+        }
+        
+        ocrRequest.recognitionLevel = .accurate
+        ocrRequest.recognitionLanguages = ["en-US", "en-GB"]
+        ocrRequest.usesLanguageCorrection = true
     }
 }
 
@@ -72,8 +140,10 @@ extension ViewController: VNDocumentCameraViewControllerDelegate {
             return
         }
         
-        print("save scan")
-        dismiss(animated: true, completion: nil)
+        // save scan
+        scanImageView.image = scan.imageOfPage(at: 0)
+        processImage(scan.imageOfPage(at: 0))
+        controller.dismiss(animated: true)
     }
     
     func documentCameraViewControllerDidCancel(_ controller: VNDocumentCameraViewController) {
